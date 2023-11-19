e40b1a8f02bd589cc71a2d2df1229c4cbc0950e9
Rosya-edwica <yunoshev.dev@gmail.com>
2023-11-19 17:28:04 +0300

    добавил модель вакансии

---
+++ internal/models/vacancy.go
@@ -0,0 +1,19 @@
+package models
+
+type Vacancy struct {
+	Id         string   `json:"id"`
+	Name       string   `json:"name"`
+	City       string   `json:"city"`
+	Platform   string   `json:"platform"`
+	Url        string   `json:"url"`
+	SalaryFrom int      `json:"salary_from"`
+	SalaryTo   int      `json:"salary_to"`
+	Currency   string   `json:"currency"`
+	Company    string   `json:"company"`
+	Skills     []string `json:"skills"`
+}
+
+type QueryVacancies struct {
+	Query       string    `json:"skill"`
+	VacancyList []Vacancy `json:"materials"`
+}
