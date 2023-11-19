13de215b94ceeb27ef4f8494bf04615093b82a2a
Rosya-edwica <yunoshev.dev@gmail.com>
2023-11-19 17:35:24 +0300

    просто читаем конфиги бд и сервера, после чего запускаем сам сервер

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
+	"github.com/Rosya-edwica/api.edwica/config"
 	"github.com/Rosya-edwica/api.edwica/internal/database"
 	"github.com/Rosya-edwica/api.edwica/internal/server"
-	"github.com/ilyakaznacheev/cleanenv"
 )
 
 func main() {
-	dbConfig := LoadDBConfig()
-	httpConfig := LoadHTTPConfig()
-	fmt.Println(dbConfig)
-	fmt.Println(httpConfig)
-	_, err := database.New(dbConfig)
+	dbConfig, err := config.LoadDBConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	httpConfig, err := config.LoadHTTPConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = database.New(dbConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	server := server.NewServer(httpConfig)
 	server.Run()
-}
-
-func LoadDBConfig() *database.Config {
-	var cfg database.Config
-	err := cleanenv.ReadConfig("config/configdb.yaml", &cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return &cfg
-}
-
-func LoadHTTPConfig() *server.Config {
-	var cfg server.Config
-	err := cleanenv.ReadConfig("config/confighttp.yaml", &cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return &cfg
 }
